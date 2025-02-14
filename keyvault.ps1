bf3aaa3e5d6e128a7c1fc74d06a821cd2332e705
Roelf Zomerman <rozome@microsoft.com>
2025-02-14 21:48:50 +0400

    Add files via upload

---
+++ keyvault.ps1
@@ -0,0 +1,170 @@
+
+param (
+    [Parameter(Mandatory= $False)]$basename,
+    [Parameter(Mandatory)]$region,
+    [Parameter(Mandatory= $False)]$vmSize = "Standard_DC2as_v5",
+    [Parameter(Mandatory = $False)]$vnetname,
+    [Parameter(Mandatory = $False)]$vmsubnetname,
+    [Parameter(Mandatory = $False)][pscredential]$Creds,
+    [Parameter(Mandatory = $False)]$KeyURL,
+    [Parameter(Mandatory = $False)]$akvname,
+    [Parameter(Mandatory = $False)]$keyName,
+    [Parameter(Mandatory = $False)][ValidateSet("MHSM", "KeyVault")][string]$akvtype,
+    [Parameter(Mandatory = $False)]$resgrp,
+    [Parameter(Mandatory = $False)]$vmname
+)  
+
+
+#setting up the build
+$basename = $basename + -join ((97..122) | Get-Random -Count 5 | % {[char]$_}) # basename + 5 random lower-case letters
+If (!($vmname)){$vmname = $basename} # name of the VM, copied from $basename, or customise it here
+If (!($resgrp)){$resgrp =  $basename} # name of the resource group where all resources will be created, copied from $basename
+If (!($KeyURL -or $akvname)){$akvname = $basename + "akv"}    # name of the Key Vault
+If (!($KeyURL-or $keyname)){$keyname = $basename + "-cmk-key"} #Name of the key in the Key Vault
+If (!($vnetname)){$vnetname = $vmname + "vnet"} # name of the VNET
+If (!($vmsubnetname)){$vmsubnetname = $basename + "vmsubnet"} # name of the VNET subnet
+If (!($desname)){$desname = $basename + "-des"} # name of the disk encryption set
+
+
+
+#when a Key URL is provided, we can extract the vault name from the key directly
+If ($KeyURL){
+    if ($KeyURL -match "vault.azure"){
+        $akvtype = "KeyVault"
+        $akvname = ($KeyURL -split ".vault.azure" -split "//")[1]
+        $keyname=($KeyURL -split "/keys/" -split "/")[3]
+        $desname=($keyname + "-des")
+    }elseif($KeyURL -match ".managedhsm.azure.net"){
+        $akvtype = "MHSM"
+        $akvname = ($KeyURL -split ".managedhsm.azure.net" -split "//")[1]
+        $keyname=($KeyURL -split "/keys/" -split "/")[3]
+        $desname=($keyname + "-des")
+    }else{
+        Write-host "Wrong KeyURL"
+        #exit
+    }
+}
+
+
+
+$vmSecurityType = "ConfidentialVM";
+$diskEncryptionType = "ConfidentialVmEncryptedWithCustomerKey";
+$secureEncryptGuestState = "DiskWithVMGuestState";
+
+#Get username of logged-in Azure user to grant access
+$tmp = Get-AzContext
+$ownername = $tmp.Account.Id
+
+
+        #Cosmetic stuff
+        write-host ""
+        write-host ""
+        write-host "                               _____        __                                " -ForegroundColor Green
+        write-host "     /\                       |_   _|      / _|                               " -ForegroundColor Yellow
+        write-host "    /  \    _____   _ _ __ ___  | |  _ __ | |_ _ __ __ _   ___ ___  _ __ ___  " -ForegroundColor Red
+        write-host "   / /\ \  |_  / | | | '__/ _ \ | | | '_ \|  _| '__/ _' | / __/ _ \| '_ ' _ \ " -ForegroundColor Cyan
+        write-host "  / ____ \  / /| |_| | | |  __/_| |_| | | | | | | | (_| || (_| (_) | | | | | |" -ForegroundColor DarkCyan
+        write-host " /_/    \_\/___|\__,_|_|  \___|_____|_| |_|_| |_|  \__,_(_)___\___/|_| |_| |_|" -ForegroundColor Magenta
+        write-host "     "
+        write-host " This script deploys a CVM with all required components" -ForegroundColor "Green"
+        write-host " You have selected" -ForegroundColor "Green"
+        write-host "  - VM name:" -NoNewline -ForegroundColor "Blue"
+        write-host $vmname -ForegroundColor "Cyan"
+        write-host "  - VM Size:" -NoNewline -ForegroundColor "Blue"
+        write-host $vmsize -ForegroundColor "Cyan"
+        write-host "  - Vnet Name:" -NoNewline -ForegroundColor "Blue"
+        write-host $vnetName -ForegroundColor "Cyan"
+        write-host "  - Subnet Name:" -NoNewline -ForegroundColor "Blue"
+        write-host $vmsubnetname -ForegroundColor "Cyan"
+        write-host "  - Resource Group:" -NoNewline -ForegroundColor "Blue"
+        write-host $resgrp -ForegroundColor "Cyan"
+        write-host "  - KeyVault Name:" -NoNewline -ForegroundColor "Blue"
+        write-host $akvname -ForegroundColor "Cyan"
+        write-host "  - KeyVault Type:" -NoNewline -ForegroundColor "Blue"
+        write-host $akvtype -ForegroundColor "Cyan"
+        write-host "  - Key Name:" -NoNewline -ForegroundColor "Blue"
+        write-host $keyname -ForegroundColor "Cyan"
+        if ($KeyURL) {
+            write-host "  - KeyURL:" -NoNewline -ForegroundColor "Blue"
+            write-host $KeyURL -ForegroundColor "Cyan"
+        }
+        
+        
+        write-host "  - Credentials:" -ForegroundColor "Blue"
+        If ($Creds) {
+            #validate that creds are correctly specified
+        
+            Write-host "   - Username:"  -ForegroundColor Blue -NoNewline
+            write-host $creds.username -ForegroundColor Cyan
+            write-host "   - Password:" -NoNewline -ForegroundColor "Blue"
+            Write-Host " <provided>" -ForegroundColor Cyan
+            If ($creds.Password.GetType().name -ne "SecureString"){
+                Write-Host " Creds are not correct" -ForegroundColor Red
+                Read-Host -Prompt "Press CRTL-X to exit"
+            }
+        }else{
+            Write-Host "  - not specified, generating"  -ForegroundColor Yellow
+            $creds=GenerateCreds
+        }
+        
+        write-host ""
+        write-host ""
+
+#Actual Script Logica
+#validate if an existing VM exists
+    If (get-azvm -Name $vmname){
+        Write-Output "VM already exists"
+    }else{
+        if(!($KeyURL)){
+            $KeyVault=CreateKeyVault -Type $akvtype -akvname $akvname -region $region -resgrp $resgrp -ownername $ownername
+        }
+
+        write-host "CVM Operator AppID check: " -ForegroundColor Blue -NoNewline
+        If (ValidateCVMOperator){
+            write-host "ok" -ForegroundColor Green
+        }
+        #write-host "-akvname $akvname"
+        #write-host "-Type $akvtype "
+        #write-host "-ownername $ownername "
+        #write-host "-keyname $keyname"
+        write-host "KeyVault access " -NoNewline -ForegroundColor Blue
+        $key=ValidateKey -akvname $akvname -Type $akvtype -ownername $ownername -keyname $keyname;
+        if ($key.id){
+            write-host $key.id -ForegroundColor Cyan
+        }else {
+            Read-Host -Prompt "Press CRTL-X to exit"
+        }
+        $keyID=$key.id
+        $diskencset=CreateDiskEncryptionSet -keyId $keyId -KeyName $keyName -region $region -resgrp $resgrp -desname $desname -akvname $akvname -EncryptionType $diskEncryptionType;
+        $SubnetID=ValidateVNET -vnetname $vnetname -subnetName $vmsubnetname -region $region -resgrp $resgrp;
+        write-host "subnetID:" -ForegroundColor Blue -NoNewline
+        write-host $subnetID -ForegroundColor Cyan
+        $nicId=CreateNIC -subnetId $SubnetID -resgrp $resgrp -region $region -vmname $vmname;
+
+
+        $diskencset=get-azdiskencryptionset -Name $desname -ResourceGroupName $resgrp
+        ##OVERVIEW##
+ 
+
+        Write-Host "Creating VM Config"
+        Write-Host "- VMName, VMSize, " -NoNewline
+        $VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $vmSize;
+        Write-Host "Operating System, Credentials, Updates" -NoNewline
+        $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $vmname -Credential $creds -ProvisionVMAgent -EnableAutoUpdate;
+        Write-Host ", Source Image" -NoNewline
+        $VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'windowsserver' -Skus '2022-datacenter-smalldisk-g2' -Version "latest";
+        Write-Host ", NIC" -NoNewline
+        $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $nicId;
+        Write-Host ", OS Disk" -NoNewline
+        $VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -StorageAccountType "StandardSSD_LRS" -CreateOption "FromImage" -SecurityEncryptionType $secureEncryptGuestState -SecureVMDiskEncryptionSet $diskencset.id;
+        Write-Host ", Security Profile" -NoNewline
+        $VirtualMachine = Set-AzVmSecurityProfile -VM $VirtualMachine -SecurityType $vmSecurityType;
+        Write-Host ", Secure Boot" -NoNewline
+        $VirtualMachine = Set-AzVmUefi -VM $VirtualMachine -EnableVtpm $true -EnableSecureBoot $true;
+        Write-Host ", Diagnostic Settings"
+        #$VirtualMachine = Set-AzVMBootDiagnostic -VM $VirtualMachine -disable #disable boot diagnostics, you can re-enable if required
+
+        Write-Host "Building VM" -ForegroundColor Green
+        New-AzVM -ResourceGroupName $resgrp -Location $region -Vm $VirtualMachine
+        #$vm = Get-AzVm -ResourceGroupName $resgrp -Name $vmname;
+    }
---
+++ keyvault.psm1
@@ -0,0 +1,308 @@
+Function CreateKeyVault($Type,$akvname,$region,$resgrp,$ownername){
+# Function to create a KeyVault or Managed HSM and returns the keyvault object
+# Create Key Vault
+If ($Type -eq "KeyVault"){
+    #need to check if exists
+    $keyvault=New-AzKeyVault -Name $akvname -Location $region -ResourceGroupName $resgrp -Sku Premium -EnabledForDiskEncryption -DisableRbacAuthorization -SoftDeleteRetentionInDays 10 -EnablePurgeProtection;
+    return $keyvault
+}elseif($Type -eq "MHSM"){
+    # Try to resolve the hostname
+    $Hostname=($akvname + ".managedhsm.azure.net")
+    write-host "Validating Managed HSM: "  -ForegroundColor Blue -NoNewline
+    write-host $Hostname -ForegroundColor Cyan
+        $result = [System.Net.Dns]::GetHostAddresses($Hostname)
+        if ($result) {
+            Write-Host "Managed HSM name already exists" -foregroundcolor Green -NoNewline
+            $keyvault=Get-AzKeyVaultManagedHsm -Name $akvname 
+            $status=($keyvault | select ProvisioningState).ProvisioningState
+            If ($status -eq "Provisioning") {
+                Write-host " still being provisioned, please wait for it to complete" -foregroundcolor orange
+                $confirmation = Read-Host "Press CRTL-X to cancel this script"
+            } else {
+                Write-Host " and provisioned" -foregroundcolor Green -NoNewline
+                
+                ActivateMHSM -akvname $akvname
+                return $keyvault
+            }
+            
+        } else {
+            Write-Host "Creating Managed HSM"
+            Write-Host "!!WARNING: The Managed HSM will be created with 7 days soft delete and purge protection enabled - this will incur costs, even after deletion!!"
+            # Prompt user for confirmation
+            $confirmation = Read-Host "Press Y to continue"
+            if ($confirmation -ne 'Y') {
+                Write-Host "Operation cancelled."
+                #exit
+            }
+            #ADDED FOR COST CONTROL
+            $tags = @{"CostControl"="Ignore"}
+            $keyvault=New-AzKeyVaultManagedHsm -ResourceGroupName $resgrp -Name $akvname -Sku StandardB1 -Location $region -Administrator $ownername -SoftDeleteRetentionInDays 7 -EnablePurgeProtection -tags $tags
+             Write-Output "Creating the managed HSM Instance.. please be patient"
+            Start-Sleep -Seconds 30
+            $status=(Get-AzKeyVaultManagedHsm -Name $akvname | select ProvisioningState).ProvisioningState
+            While ($status -eq "Provisioning") {
+                Write-Host "."  -NoNewline -ForegroundColor Yellow
+                Start-Sleep -Seconds 10
+                $status=(Get-AzKeyVaultManagedHsm -Name $akvname | select ProvisioningState).ProvisioningState
+            }
+            ActivateMHSM -akvname $akvname
+            return $keyvault
+        }
+ 
+
+    }
+}
+
+Function ActivateMHSM($akvname){
+    $MyHSM=(Get-AzKeyVaultManagedHsm -Name $akvname)
+    if ($MyHSM.SecurityDomain.ActivationStatus -eq "NotActivated") { 
+        Write-host " - Activating Managed HSM" -ForegroundColor Yellow
+        GenerateCerts
+        #activate the managed HSM with the generated certs
+        $path=((get-location).path + "\")
+        Export-AzKeyVaultSecurityDomain -Name $akvname -Certificates ($path + "cert1.cer"), ($path + "cert2.cer"), ($path + "cert3.cer") -OutputPath "MHSMsd.ps.json" -Quorum 2
+        start-sleep -Seconds 30
+        if ($MyHSM.SecurityDomain.ActivationStatus -eq "Activated"){
+            Write-host "Managed HSM activated" -ForegroundColor Green
+        }
+    } elseif($MyHSM.SecurityDomain.ActivationStatus -eq "Active") {
+        Write-host " and activated" -ForegroundColor Green
+    }else{
+        Write-host "Managed HSM status unkown" -ForegroundColor Red
+        Read-Host -Prompt "Press CRTL-X to exit"
+    }
+}
+
+Function CreateDiskEncryptionSet($keyName,$KeyID,$region,$resgrp,$desname, $akvname, $keyvault, $Type, $encryptionType){
+    #if a disk encryption set already exists, we will check it
+    If ($diskEncryptionSet=Get-AzDiskEncryptionSet -ResourceGroupName $resgrp -Name $desname -ErrorAction SilentlyContinue){
+        write-host "Disk Encryption Set already exists - validating key" -ForegroundColor Yellow -NoNewline
+        $SourceKey=$diskEncryptionSet.ActiveKey.KeyUrl
+        $SourceVault=$diskEncryptionSet.ActiveKey.SourceVault
+        $SourceKeyName=($SourceKey -split "/keys/" -split "/")[3]
+        $SourceVaultName= ($SourceVault.id -split "/")[$_.count -1]
+        If (!($SourceVaultName -eq $akvname -and $SourceKeyName -eq $keyName)){
+            write-host " - key is different!" -ForegroundColor Red
+            Read-Host -Prompt "Press CRTL-X to exit"
+        }else{
+            write-host " - OK" -ForegroundColor Green
+        }
+        $diskEncryptionSetID=$diskEncryptionSet.id
+        return $diskEncryptionSetID
+    }else{
+        write-host "Validating new Disk Encryption Set: " -NoNewline -ForegroundColor Blue
+        write-host $desname -ForegroundColor Cyan
+      
+        # Create Disk Encryption Set
+        #creating new User Assigned Identity to access the key
+        write-host "- validating user assigned identity: "  -NoNewline
+        write-host $keyName -NoNewline -ForegroundColor Cyan
+        If (!($identity=Get-AzUserAssignedIdentity -Name $keyName -ResourceGroupName $resgrp -ErrorAction SilentlyContinue)){
+            write-host " - creating" -ForegroundColor Yellow
+            $identity=New-AzUserAssignedIdentity -ResourceGroupName $resgrp -Name $keyName -Location $region
+            start-sleep -Seconds 5
+        }
+        #assigning permissions to the key:
+        write-host "- validating permissions on key for user: "  -NoNewline -ForegroundColor Blue
+        write-host $identity.name -ForegroundColor Cyan
+        ValidateHSMRoles2 -akvname $akvname -identity $identity.ClientId -role "Managed HSM Crypto Service Encryption User" -scope ("/keys/" + $keyName)
+        ValidateHSMRoles2 -akvname $akvname -identity $identity.ClientId -role "Managed HSM Crypto User" -scope ("/keys/" + $keyName)
+        ValidateHSMRoles2 -akvname $akvname -identity $identity.ClientId -role "Managed HSM Crypto Service Release User" -scope ("/keys/" + $keyName)
+        
+    #NEED TO MAKE ADJUSTMENTS IN CASE THIS DOES NOT EXIST YET
+        $org=Get-AzADServicePrincipal -DisplayName "Confidential VM Orchestrator"
+        
+        write-host "- validating permissions on key for: " -NoNewline -ForegroundColor Blue
+        write-host $org.DisplayName -ForegroundColor Cyan
+        ValidateHSMRoles2 -akvname $akvname -identity $org.AppId -role "Managed HSM Crypto Service Release User" -scope ("/keys/" + $keyName)
+
+        #New-AzKeyVaultRoleAssignment -HsmName $akvname -RoleName "Managed HSM Crypto Service Encryption User" -scope ("/keys/" + $keyName) -ObjectId $identity.ClientId
+        #New-AzKeyVaultRoleAssignment -HsmName $akvname -RoleName "Managed HSM Crypto User" -scope ("/keys/" + $keyName) -ObjectId $identity.ClientId
+        
+        If ($Type -eq "KeyVault"){
+            $keyvault = Get-AzKeyVault -Name $akvname
+        }else{
+            write-host "- validating Managed HSM access " -NoNewline
+            $keyvault = Get-AzKeyVaultManagedHsm -Name $akvname
+            write-host $keyvault.resourceid -ForegroundColor Green
+        }
+        If (!($keyvault)){
+            Write-Output "KeyVault not reachable"
+            Read-Host -Prompt "Press Enter to exit"
+        }
+
+        $userAssignedIdentities = @{$identity.id = @{}};
+        write-host "- creating new disk encryption set config"
+        $diskEncryptionSetConfig=New-AzDiskEncryptionSetConfig -Location $region -IdentityType UserAssigned -SourceVaultId $keyvault.resourceid -KeyUrl $keyId -UserAssignedIdentity $userAssignedIdentities -EncryptionType $encryptionType
+        write-host "- creating new disk encryption set:" $desname -ForegroundColor Yellow
+        $diskEncryptionSet = New-AzDiskEncryptionSet -ResourceGroupName $resgrp -Name $desname -InputObject $diskEncryptionSetConfig
+        Write-Host "Disk Encryption Set created" -ForegroundColor Green
+        $diskEncryptionSetID=$diskEncryptionSet.id
+        return $diskEncryptionSetID
+    }
+}
+
+
+Function ValidateHSMRoles2($akvname,$identity, $role, $scope){
+    Write-host "- validating $scope access for:"  -NoNewline
+    write-host $identity -NoNewline -ForegroundColor Cyan
+    $ExistingRoles=Get-AzKeyVaultRoleAssignment -HsmName $akvname -scope $scope
+    if ($identity -match "@"){
+        $UPN=$true
+    }
+    If ($ExistingRoles | where-Object {$_.DisplayName.Contains($identity)} |Where-Object {$_.RoleDefinitionName.Contains($role)}){
+        Write-Host " - role is assigned" -ForegroundColor Green
+    }else{
+        Write-Host " - assigning" -ForegroundColor Yellow
+            If ($UPN){
+                New-AzKeyVaultRoleAssignment -HsmName $akvname -RoleName $role -SignInName $identity -scope $scope
+            }else{
+                New-AzKeyVaultRoleAssignment -HsmName $akvname -RoleName $role -ApplicationID $identity -scope $scope
+            }
+    }
+}
+
+Function ValidateHSMRoles($akvname,$ownername){
+    $ExistingRoles=Get-AzKeyVaultRoleAssignment -HsmName $akvname -SignInName $ownername -scope /keys
+    If ($ExistingRoles | Where-Object {$_.RoleDefinitionName.Contains("Managed HSM Crypto User")}){
+        Write-Output "Role is assigned"
+    }else{
+        New-AzKeyVaultRoleAssignment -HsmName $akvname -RoleName "Managed HSM Crypto User" -SignInName $ownername -scope /keys
+        }
+}
+Function ValidateKey($akvname,$Type,$ownername, $keyname){
+    #Function validates if a key exists, if not, it will create one (if we have permissions) function returns the key object
+
+    #creating a new managed user identity to access the key - this way each disk encryption set will have its own identity
+    
+    if($Type -eq "KeyVault"){
+    #Standard KeyVault entries to be made
+
+    }elseif($Type -eq "MHSM"){
+    #validating access to the keys in managed HSM
+    #ValidateHSMRoles -akvname $akvname -ownername $ownername
+    ValidateHSMRoles2 -akvname $akvname -identity $ownername -role "Managed HSM Crypto User" -scope /keys
+
+    #validating if a key already exists
+    if ($key=Get-AzKeyVaultKey -HsmName $akvname -Name $keyname) {
+        Write-Host "Key exists"
+        return $key
+    }else{
+        #generate new key
+        write-host "- creating new key: "  -ForegroundColor Yellow -NoNewline
+        $key=Add-AzKeyVaultKey -HsmName $akvname -Name $keyname -KeyType RSA -Size 3072 -Exportable -ReleasePolicyPath C:\Scripts\MHSM\release.json 
+        return $key
+    }
+
+   }else{
+         Write-Output "Invalid vault"
+   }
+}
+
+Function ValidateVNET($vnetname, $subnetName, $region, $resgrp){
+    #Validates if a network exists, else it will create one and return the subnet ID
+    Write-host "Validating" -NoNewline
+    $vnet = Get-AzVirtualNetwork -Name ($vnetname)
+    If (!($Vnet)){
+        Write-host " - creating vnet ($vnetname) with 10.0.0.0/24 and subnet ($subnetName) 10.0.0.0/26" -ForegroundColor Yellow
+        $subnet = New-AzVirtualNetworkSubnetConfig -Name ($subnetName) -AddressPrefix "10.0.0.0/26";
+        $vnet = New-AzVirtualNetwork -Force -Name ($vnetname) -ResourceGroupName $resgrp -Location $region -AddressPrefix "10.0.0.0/24" -Subnet $subnet;
+        #$vnet = Get-AzVirtualNetwork -Name ($vnetname) -ResourceGroupName $resgrp;
+        $subnetId = $vnet.Subnets[0].Id;    
+        return $subnetId  
+    }else{
+        write-host (" - " + $vnet.name) -ForegroundColor Green -NoNewline
+        If ($Vnet.location -ne $region){
+            Write-host "VNET is in a different region" -ForegroundColor red
+            #exit
+        }
+        If ($subnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet){
+            Write-Host (" - " + $subnet.Name) -ForegroundColor Green   
+            $subnetId = $subnet.Id
+        }else{
+            Write-Host "Subnet does not exist" -ForegroundColor Red
+            #exit
+
+        }
+    return $subnetId   
+    }
+}
+
+Function CreateNIC($subnetId, $resgrp, $region, $vmname){
+    #Create the NIC and link it to the subnet
+    Write-host "Validating NIC " -NoNewline
+    $nicname = $vmname + "-nic1"
+    $nic=Get-AzNetworkInterface -name $nicname
+    If ($nic){
+        Write-Host ("- already exists - " + $nic.Name) -ForegroundColor Green
+        $nicId = $nic.Id
+        return $nicId
+    }else{
+        Write-Host "- creating" -ForegroundColor Yellow
+        $nic = New-AzNetworkInterface -Name $nicname -ResourceGroupName $resgrp -Location $region -SubnetId $subnetId
+        $nicId = $nic.Id
+        return $nicId
+    }
+}
+Function GenerateCerts(){
+    $certNames = @("cert1", "cert2", "cert3")
+    $path=((get-location).path + "\")
+    $certStoreLocation = "Cert:\CurrentUser\My"
+    
+    foreach ($name in $certNames) {
+        # Generate self-signed certificate
+        $cert = New-SelfSignedCertificate -Subject "CN=$name" -KeyAlgorithm RSA -KeyLength 2048 -CertStoreLocation $certStoreLocation
+    
+        # Export the certificate (Public Key) as Base64 PEM
+        #$certPath = ($path + "$name-public.pem")
+        Export-Certificate -Cert $cert -FilePath ($path + "$name.cer")
+        #$certBase64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes(($path + "$name.cer"))) -replace ".{64}", "$&`n"
+        #Set-Content -Path $certPath -Value "-----BEGIN CERTIFICATE-----`n$certBase64`n-----END CERTIFICATE-----" -NoNewline
+    }
+    
+    Write-Host "Certificates and keys have been successfully generated!"
+    Write-Host "Note that the full certificates (including private keys required for restore) are stored in the user Certificate Store."
+    
+    }
+
+Function GenerateCreds(){
+    #Function to generate credentials for the VM
+    $vmusername = "azureuser" # you can adjust this if you want
+    $vmadminpassword = -join ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%".ToCharArray() | Get-Random -Count 40) # build a random password - note you can't get it back afterwards
+    #create a credential object
+    write-host "----------------------------------------------------------------------------------------------------------------"
+    write-host "IMPORTANT - no creds where specified - building random creds for the VM"
+    write-host "VM admin username is " $vmusername
+    write-host "passsword for the VM is " $vmadminpassword " - save this now as you CANNOT retrieve it later"
+    write-host "----------------------------------------------------------------------------------------------------------------"
+    $securePassword = ConvertTo-SecureString -String $vmadminpassword -AsPlainText -Force # this could probably be done better inline rather than via a variable
+    $cred = New-Object System.Management.Automation.PSCredential ($vmusername, $securePassword);
+    return $cred
+}
+
+Function ValidateCVMOperator(){
+    #need to validate if the Confidential VM Orchestrator exists
+    $org=Get-AzADServicePrincipal -DisplayName "Confidential VM Orchestrator"
+    if (!($org)){
+        write-host " - The " -ForegroundColor Yellow -NoNewline
+        write-host "Confidential VM Orchestrator" -ForegroundColor Cyan -NoNewline
+        write-host " does not exist in your tenant - you will need to sign-in to the Graph to create " -ForegroundColor Yellow -NoNewline
+        Connect-Graph Application.ReadWrite.All
+        New-MgServicePrincipal -AppId bf7b6499-ff71-4aa2-97a4-f372087be7f0 -DisplayName "Confidential VM Orchestrator" 
+        start-sleep -Seconds 5
+        $org=Get-AzADServicePrincipal -DisplayName "Confidential VM Orchestrator"
+        if (!($org)){
+            write-host " SOMETIHNG WENT WRONG " -ForegroundColor RED  -NoNewline
+            write-host " please run: " -ForegroundColor RED  -NoNewline
+            write-host "Connect-Graph -Tenant 'your tenant ID' Application.ReadWrite.All" -ForegroundColor Yellow
+            write-host "New-MgServicePrincipal -AppId bf7b6499-ff71-4aa2-97a4-f372087be7f0 -DisplayName 'Confidential VM Orchestrator'" -ForegroundColor Yellow
+            Read-Host -Prompt "Press CRTL-X to exit"
+        }
+        return $true
+    }elseif($org.AppId -eq 'bf7b6499-ff71-4aa2-97a4-f372087be7f0'){
+        return $true
+    }else{
+        write-host "Something is wrong with the Confidential VM Orchestrator app" -ForegroundColor Red
+        Read-Host -Prompt "Press CRTL-X to exit"
+    }
+}
